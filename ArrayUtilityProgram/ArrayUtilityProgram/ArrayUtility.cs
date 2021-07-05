4707c3df1e8ff28c4f220a3571b8079d6520400c
navyashrees29 <navya9529@gmail.com>
2021-07-05 16:04:54 +0530

    upload

---
+++ ArrayUtilityProgram/ArrayUtilityProgram/ArrayUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayUtilityProgram
+{
+    class ArrayUtility
+    {
+        public static void Resize(ref int[] array, int newSize)
+        {
+            array = new int[newSize];
+            int[] array1 = new int[newSize];
+          
+           for (int i=0;i<array.Length;i++)
+            {
+                array[i] = array1[i];
+            }
+
+        }
+    }
+}
